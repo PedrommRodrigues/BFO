43efa81cee64169948ba196c1b0bae43e5c4073f
Pedro Rodrigues <pedrommarquesrodrigues@gmail.com>
2023-12-06 19:21:45 +0000

    Add button to toggle visibility of additional text and css fix

--- js/main.js
+++ js/main.js
@@ -2,3 +2,18 @@ function toggleMenu() {
   const menu = document.querySelector(".hamburger");
   menu.classList.toggle("is-active");
 }
+
+function expandHanddler() {
+  const expandedText = document.querySelector("#expText");
+  expandedText.classList.toggle("is-hidden");
+
+  let spanText = document.querySelector(".hide-button");
+
+  if (spanText.innerText == "Ver menos...") {
+    spanText.innerText = "Ver mais...";
+  } else {
+    spanText.innerText = "Ver menos...";
+  }
+
+  console.log(spanText.innerText);
+}
