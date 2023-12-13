0951076e87af2ecad2952732de5aa37e6b6f5baf
Pedro Miguel Marques Rodrigues <pedrommarquesrodrigues@gmail.com>
2023-12-13 19:07:20 +0000

    Added all images to "Historial"

--- js/main.js
+++ js/main.js
@@ -17,3 +17,15 @@ function expandHanddler() {
     spanText.innerText = "Ver menos...";
   }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const container = document.getElementById("imageContainer");
+  const numberOfImages = 52;
+
+  for (let i = 1; i <= numberOfImages; i++) {
+    const img = document.createElement("img");
+    img.src = `Images/historial/${i}.png`;
+    img.alt = "fotografia da banda";
+    container.appendChild(img);
+  }
+});
