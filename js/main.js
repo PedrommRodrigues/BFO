30eb9cec3a8896c278885b5334e2033d2aabd240
Pedro Miguel Marques Rodrigues <pedrommarquesrodrigues@gmail.com>
2024-01-25 18:16:46 +0000

    Add event listener for checkbox change
    Implemented a JavaScript function to handle the change event of the checkbox with ID "checkbox1". When the checkbox is unchecked, it now performs a specific action [describe the action if relevant]. This improves the functionality related to checkbox interactions.

--- js/main.js
+++ js/main.js
@@ -29,3 +29,18 @@ document.addEventListener("DOMContentLoaded", function () {
     container.appendChild(img);
   }
 });
+
+const payAtHome = document.getElementById("payHome");
+const payAtHeadquarters = document.getElementById("payHeadquarters");
+
+payAtHome.addEventListener("change", function () {
+  if (payAtHome.checked) {
+    payAtHeadquarters.checked = false;
+  }
+});
+
+payAtHeadquarters.addEventListener("change", function () {
+  if (payAtHeadquarters.checked) {
+    payAtHome.checked = false;
+  }
+});
